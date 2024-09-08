9c4c7b1218a6b7021a3ce06fcb95c6403151533b
igor-bzovsky <igor.bzovsky@gmail.com>
2024-09-08 15:14:01 +0300

    Add map operations

---
+++ src/Rop.NET/Map.cs
@@ -0,0 +1,38 @@
+﻿namespace Rop.NET
+{
+    public abstract partial class Result<TSuccess, TFailure>
+    {
+        public Result<TNewSuccess, TFailure> Map<TNewSuccess>(Func<TSuccess, TNewSuccess> mapFunction)
+        {
+            _ = mapFunction ?? throw new ArgumentNullException(nameof(mapFunction));
+            return MapInternal(mapFunction);
+        }
+
+        protected abstract Result<TNewSuccess, TFailure> MapInternal<TNewSuccess>(Func<TSuccess, TNewSuccess> mapFunction);
+
+        #region Success & Failure
+        private sealed partial class Success : Result<TSuccess, TFailure>
+        {
+            override protected Result<TNewSuccess, TFailure> MapInternal<TNewSuccess>(Func<TSuccess, TNewSuccess> mapFunction) => Result.Succeed(mapFunction(Value));
+        }
+        private sealed partial class Failure : Result<TSuccess, TFailure>
+        {
+            override protected Result<TNewSuccess, TFailure> MapInternal<TNewSuccess>(Func<TSuccess, TNewSuccess> mapFunction) => Result.Fail(Error);
+        }
+        #endregion
+    }
+
+    public static class MapExtensions
+    {
+        public static async Task<Result<TNewSuccess, TFailure>> Map<TNewSuccess, TSuccess, TFailure>(
+            this Task<Result<TSuccess, TFailure>> taskResult,
+            Func<TSuccess, TNewSuccess> mapFunction)
+        {
+            _ = taskResult ?? throw new ArgumentNullException(nameof(taskResult));
+            _ = mapFunction ?? throw new ArgumentNullException(nameof(mapFunction));
+
+            var result = await taskResult.ConfigureAwait(false);
+            return result.Map(mapFunction);
+        }
+    }
+}
---
+++ tests/Rop.NET.Tests/MapTests.cs
@@ -0,0 +1,121 @@
+﻿using FluentAssertions;
+using Rop.NET.BaseTypes;
+using Rop.NET.Extensions;
+using Rop.NET.Tests.Common;
+
+namespace Rop.NET.Tests
+{
+    using static Errors;
+    using static Results.Strings;
+
+    public class MapTests : TestsBase
+    {
+        [Fact]
+        public void Throws_exception_when_map_function_is_null()
+        {
+            Result<Unit, Error> sut = UnitResult.Succeed();
+            Func<Unit, string> mapFunction = null!;
+
+            sut.Invoking(res => res.Map(mapFunction))
+                .Should()
+                .Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Rethrows_map_function_exception()
+        {
+            var exception = new InvalidOperationException();
+            Result<Unit, Error> sut = UnitResult.Succeed();
+            Func<Unit, string> mapFunction = _ => throw exception;
+
+            sut.Invoking(res => res.Map(mapFunction))
+                .Should()
+                .Throw<InvalidOperationException>()
+                .Which.Equals(exception);
+        }
+
+        [Fact]
+        public void Maps_success_result_to_new_success_result()
+        {
+            Result<Unit, Error> sut = UnitResult.Succeed();
+            Func<Unit, string> mapFunction = _ => Success;
+
+            var result = sut.Map(mapFunction);
+
+            AssertSuccess(result, Success);
+        }
+
+        [Fact]
+        public void Returns_initial_failure()
+        {
+            Result<Unit, Error> sut = UnitResult.Fail(UnexpectedError);
+            Func<Unit, string> mapFunction = _ => Success;
+
+            var result = sut.Map(mapFunction);
+
+            AssertFailure(result, UnexpectedError);
+        }
+    }
+
+    public class MapExtensionsTests : TestsBase
+    {
+        [Fact]
+        public async Task Throws_exception_when_task_result_is_null()
+        {
+            Task<Result<Unit, Error>> sut = null!;
+            Func<Unit, string> mapFunction = _ => Success;
+
+            var task = () => sut.Map(mapFunction);
+
+            await task.Should()
+                .ThrowAsync<ArgumentNullException>();
+        }
+
+        [Fact]
+        public async Task Throws_exception_when_map_function_is_null()
+        {
+            Task<Result<Unit, Error>> sut = UnitResult.Succeed<Error>().AsTask();
+            Func<Unit, string> mapFunction = _ => null!;
+
+            await sut.Invoking(res => res.Map(mapFunction))
+                .Should()
+                .ThrowAsync<ArgumentNullException>();
+        }
+
+        [Fact]
+        public async Task Rethrows_map_function_exception()
+        {
+            Task<Result<Unit, Error>> sut = UnitResult.Succeed<Error>().AsTask();
+            var exception = new InvalidOperationException();
+            Func<Unit, string> mapFunction = _ => throw exception;
+
+
+            var result = await sut.Invoking(res => res.Map(mapFunction))
+                .Should()
+                .ThrowAsync<InvalidOperationException>();
+            result.Which.Should().Be(exception);
+        }
+
+        [Fact]
+        public async Task Maps_success_result_to_new_success_result()
+        {
+            Task<Result<Unit, Error>> sut = UnitResult.Succeed<Error>().AsTask();
+            Func<Unit, string> mapFunction = _ => Success;
+
+            var result = await sut.Map(mapFunction.Invoke);
+
+            AssertSuccess(result, Success);
+        }
+
+        [Fact]
+        public async Task Returns_initial_failure()
+        {
+            Task<Result<Unit, Error>> sut = UnitResult.Fail(UnexpectedError).AsTask();
+            Func<Unit, string> mapFunction = _ => Success;
+
+            var result = await sut.Map(mapFunction);
+
+            AssertFailure(result, UnexpectedError);
+        }
+    }
+}
