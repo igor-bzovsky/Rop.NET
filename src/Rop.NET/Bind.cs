2ac14ca68ca173cb101ebed88c6106f8dd3bb30b
igor-bzovsky <igor.bzovsky@gmail.com>
2024-09-08 21:37:11 +0300

    Rename successFunction for bind operation

--- src/Rop.NET/Bind.cs
+++ src/Rop.NET/Bind.cs
@@ -4,35 +4,35 @@ namespace Rop.NET
 {
     public abstract partial class Result<TSuccess, TFailure>
     {
-        public Result<TNewSuccess, TFailure> Bind<TNewSuccess>(Func<TSuccess, Result<TNewSuccess, TFailure>> successFunction)
+        public Result<TNewSuccess, TFailure> Bind<TNewSuccess>(Func<TSuccess, Result<TNewSuccess, TFailure>> bindFunction)
         {
-            _ = successFunction ?? throw new ArgumentNullException(nameof(successFunction));
-            return BindInternal(successFunction);
+            _ = bindFunction ?? throw new ArgumentNullException(nameof(bindFunction));
+            return BindInternal(bindFunction);
         }
 
-        public async Task<Result<TNewSuccess, TFailure>> BindAsync<TNewSuccess>(Func<TSuccess, Task<Result<TNewSuccess, TFailure>>> successFunctionAsync)
+        public async Task<Result<TNewSuccess, TFailure>> BindAsync<TNewSuccess>(Func<TSuccess, Task<Result<TNewSuccess, TFailure>>> bindFunctionAsync)
         {
-            _ = successFunctionAsync ?? throw new ArgumentNullException(nameof(successFunctionAsync));
-            return await BindInternalAsync(successFunctionAsync).ConfigureAwait(false);
+            _ = bindFunctionAsync ?? throw new ArgumentNullException(nameof(bindFunctionAsync));
+            return await BindInternalAsync(bindFunctionAsync).ConfigureAwait(false);
         }
 
-        protected abstract Result<TNewSuccess, TFailure> BindInternal<TNewSuccess>(Func<TSuccess, Result<TNewSuccess, TFailure>> successFunction);
+        protected abstract Result<TNewSuccess, TFailure> BindInternal<TNewSuccess>(Func<TSuccess, Result<TNewSuccess, TFailure>> bindFunction);
 
-        protected abstract Task<Result<TNewSuccess, TFailure>> BindInternalAsync<TNewSuccess>(Func<TSuccess, Task<Result<TNewSuccess, TFailure>>> successFunctionAsync);
+        protected abstract Task<Result<TNewSuccess, TFailure>> BindInternalAsync<TNewSuccess>(Func<TSuccess, Task<Result<TNewSuccess, TFailure>>> bindFunctionAsync);
 
 
         #region Success & Failure
         private sealed partial class Success : Result<TSuccess, TFailure>
         {
-            override protected Result<TNewSuccess, TFailure> BindInternal<TNewSuccess>(Func<TSuccess, Result<TNewSuccess, TFailure>> successFunction) => successFunction(Value);
+            override protected Result<TNewSuccess, TFailure> BindInternal<TNewSuccess>(Func<TSuccess, Result<TNewSuccess, TFailure>> bindFunction) => bindFunction(Value);
 
-            override protected async Task<Result<TNewSuccess, TFailure>> BindInternalAsync<TNewSuccess>(Func<TSuccess, Task<Result<TNewSuccess, TFailure>>> successFunctionAsync) => await successFunctionAsync(Value).ConfigureAwait(false);
+            override protected async Task<Result<TNewSuccess, TFailure>> BindInternalAsync<TNewSuccess>(Func<TSuccess, Task<Result<TNewSuccess, TFailure>>> bindFunctionAsync) => await bindFunctionAsync(Value).ConfigureAwait(false);
         }
         private sealed partial class Failure : Result<TSuccess, TFailure>
         {
-            override protected Result<TNewSuccess, TFailure> BindInternal<TNewSuccess>(Func<TSuccess, Result<TNewSuccess, TFailure>> successFunction) => Result.Fail(Error);
+            override protected Result<TNewSuccess, TFailure> BindInternal<TNewSuccess>(Func<TSuccess, Result<TNewSuccess, TFailure>> bindFunction) => Result.Fail(Error);
 
-            override protected async Task<Result<TNewSuccess, TFailure>> BindInternalAsync<TNewSuccess>(Func<TSuccess, Task<Result<TNewSuccess, TFailure>>> successFunctionAsync)
+            override protected async Task<Result<TNewSuccess, TFailure>> BindInternalAsync<TNewSuccess>(Func<TSuccess, Task<Result<TNewSuccess, TFailure>>> bindFunctionAsync)
                 => await Result.Fail<TNewSuccess, TFailure>(Error).AsTask().ConfigureAwait(false);
         }
         #endregion
@@ -42,24 +42,24 @@ namespace Rop.NET
     {
         public static async Task<Result<TNewSuccess, TFailure>> Bind<TNewSuccess, TSuccess, TFailure>(
             this Task<Result<TSuccess, TFailure>> taskResult,
-            Func<TSuccess, Result<TNewSuccess, TFailure>> successFunction)
+            Func<TSuccess, Result<TNewSuccess, TFailure>> bindFunction)
         {
             _ = taskResult ?? throw new ArgumentNullException(nameof(taskResult));
-            _ = successFunction ?? throw new ArgumentNullException(nameof(successFunction));
+            _ = bindFunction ?? throw new ArgumentNullException(nameof(bindFunction));
 
             var result = await taskResult.ConfigureAwait(false);
-            return result.Bind(successFunction);
+            return result.Bind(bindFunction);
         }
 
         public static async Task<Result<TNewSuccess, TFailure>> BindAsync<TNewSuccess, TSuccess, TFailure>(
             this Task<Result<TSuccess, TFailure>> taskResult,
-            Func<TSuccess, Task<Result<TNewSuccess, TFailure>>> successFunctionAsync)
+            Func<TSuccess, Task<Result<TNewSuccess, TFailure>>> bindFunctionAsync)
         {
             _ = taskResult ?? throw new ArgumentNullException(nameof(taskResult));
-            _ = successFunctionAsync ?? throw new ArgumentNullException(nameof(successFunctionAsync));
+            _ = bindFunctionAsync ?? throw new ArgumentNullException(nameof(bindFunctionAsync));
 
             var result = await taskResult.ConfigureAwait(false);
-            return await result.BindAsync(successFunctionAsync).ConfigureAwait(false);
+            return await result.BindAsync(bindFunctionAsync).ConfigureAwait(false);
         }
     }
 }
--- tests/Rop.NET.Tests/BindTests.cs
+++ tests/Rop.NET.Tests/BindTests.cs
@@ -11,47 +11,47 @@ namespace Rop.NET.Tests
     public class BindTests : TestsBase
     {
         [Fact]
-        public void Throws_exception_when_success_function_is_null()
+        public void Throws_exception_when_bind_function_is_null()
         {
             Result<Unit, Error> sut = UnitResult.Succeed();
-            Func<Unit, Result<string, Error>> successFunction = null!;
+            Func<Unit, Result<string, Error>> bindFunction = null!;
 
-            sut.Invoking(res => res.Bind(successFunction))
+            sut.Invoking(res => res.Bind(bindFunction))
                 .Should()
                 .Throw<ArgumentNullException>();
         }
 
         [Fact]
-        public void Rethrows_success_function_exception()
+        public void Rethrows_bind_function_exception()
         {
             Result<Unit, Error> sut = UnitResult.Succeed();
             var exception = new InvalidOperationException();
-            Func<Unit, Result<string, Error>> successFunction = _ => throw exception;
+            Func<Unit, Result<string, Error>> bindFunction = _ => throw exception;
 
-            sut.Invoking(res => res.Bind(successFunction))
+            sut.Invoking(res => res.Bind(bindFunction))
                 .Should()
                 .Throw<InvalidOperationException>()
                 .Which.Equals(exception);
         }
 
         [Fact]
-        public void Returns_success_function_result()
+        public void Returns_bind_function_result()
         {
             Result<Unit, Error> sut = UnitResult.Succeed();
-            Func<Unit, Result<string, Error>> successFunction = _ => Result.Succeed(Success);
+            Func<Unit, Result<string, Error>> bindFunction = _ => Result.Succeed(Success);
 
-            var result = sut.Bind(successFunction);
+            var result = sut.Bind(bindFunction);
 
             AssertSuccess(result, Success);
         }
 
         [Fact]
-        public void Returns_success_function_failure()
+        public void Returns_bind_function_failure()
         {
             Result<Unit, Error> sut = UnitResult.Succeed();
-            Func<Unit, Result<string, Error>> successFunction = _ => Result.Fail(DefaultError);
+            Func<Unit, Result<string, Error>> bindFunction = _ => Result.Fail(DefaultError);
 
-            var result = sut.Bind(successFunction);
+            var result = sut.Bind(bindFunction);
 
             AssertFailure(result, DefaultError);
         }
@@ -60,9 +60,9 @@ namespace Rop.NET.Tests
         public void Returns_initial_failure()
         {
             Result<Unit, Error> sut = UnitResult.Fail(UnexpectedError);
-            Func<Unit, Result<string, Error>> successFunction = _ => Result.Succeed(Success);
+            Func<Unit, Result<string, Error>> bindFunction = _ => Result.Succeed(Success);
 
-            var result = sut.Bind(successFunction);
+            var result = sut.Bind(bindFunction);
 
             AssertFailure(result, UnexpectedError);
         }
@@ -71,24 +71,24 @@ namespace Rop.NET.Tests
     public class BindAsyncTests : TestsBase
     {
         [Fact]
-        public async Task Throws_exception_when_success_function_is_null()
+        public async Task Throws_exception_when_bind_function_is_null()
         {
             Result<Unit, Error> sut = UnitResult.Succeed();
-            Func<Unit, Task<Result<string, Error>>> successFunctionAsync = null!;
+            Func<Unit, Task<Result<string, Error>>> bindFunctionAsync = null!;
 
-            await sut.Invoking(res => res.BindAsync(successFunctionAsync))
+            await sut.Invoking(res => res.BindAsync(bindFunctionAsync))
                 .Should()
                 .ThrowAsync<ArgumentNullException>();
         }
 
         [Fact]
-        public async Task Rethrows_success_function_exception()
+        public async Task Rethrows_bind_function_exception()
         {
             Result<Unit, Error> sut = UnitResult.Succeed();
             var exception = new InvalidOperationException();
-            Func<Unit, Task<Result<string, Error>>> successFunctionAsync = _ => throw exception;
+            Func<Unit, Task<Result<string, Error>>> bindFunctionAsync = _ => throw exception;
 
-            var result = await sut.Invoking(res => res.BindAsync(successFunctionAsync))
+            var result = await sut.Invoking(res => res.BindAsync(bindFunctionAsync))
                 .Should()
                 .ThrowAsync<InvalidOperationException>();
 
@@ -96,25 +96,23 @@ namespace Rop.NET.Tests
         }
 
         [Fact]
-        public async Task Returns_success_function_result()
+        public async Task Returns_bind_function_result()
         {
             Result<Unit, Error> sut = UnitResult.Succeed();
-            Func<Unit, Task<Result<string, Error>>> successFunctionAsync =
-                _ => Result.Succeed<string, Error>(Success).AsTask();
+            Func<Unit, Task<Result<string, Error>>> bindFunctionAsync = _ => Result.Succeed<string, Error>(Success).AsTask();
 
-            var result = await sut.BindAsync(successFunctionAsync);
+            var result = await sut.BindAsync(bindFunctionAsync);
 
             AssertSuccess(result, Success);
         }
 
         [Fact]
-        public async Task Returns_success_function_failure()
+        public async Task Returns_bind_function_failure()
         {
             Result<Unit, Error> sut = UnitResult.Succeed();
-            Func<Unit, Task<Result<string, Error>>> successFunctionAsync =
-                _ => Result.Fail<string, Error>(DefaultError).AsTask();
+            Func<Unit, Task<Result<string, Error>>> bindFunctionAsync = _ => Result.Fail<string, Error>(DefaultError).AsTask();
 
-            var result = await sut.BindAsync(successFunctionAsync);
+            var result = await sut.BindAsync(bindFunctionAsync);
 
             AssertFailure(result, DefaultError);
         }
@@ -123,10 +121,9 @@ namespace Rop.NET.Tests
         public async Task Returns_initial_failure()
         {
             Result<Unit, Error> sut = Result.Fail(UnexpectedError);
-            Func<Unit, Task<Result<string, Error>>> successFunctionAsync =
-                _ => Result.Succeed<string, Error>(Success).AsTask();
+            Func<Unit, Task<Result<string, Error>>> bindFunctionAsync = _ => Result.Succeed<string, Error>(Success).AsTask();
 
-            var result = await sut.BindAsync(successFunctionAsync);
+            var result = await sut.BindAsync(bindFunctionAsync);
 
             AssertFailure(result, UnexpectedError);
         }
@@ -138,57 +135,57 @@ namespace Rop.NET.Tests
         public async Task Throws_exception_when_task_result_is_null()
         {
             Task<Result<Unit, Error>> sut = null!;
-            Func<Unit, Result<string, Error>> successFunction = _ => Result.Succeed(Success);
+            Func<Unit, Result<string, Error>> bindFunction = _ => Result.Succeed(Success);
 
-            var task = () => sut.Bind(successFunction);
+            var task = () => sut.Bind(bindFunction);
 
             await task.Should()
                 .ThrowAsync<ArgumentNullException>();
         }
 
         [Fact]
-        public async Task Throws_exception_when_success_function_is_null()
+        public async Task Throws_exception_when_bind_function_is_null()
         {
             Task<Result<Unit, Error>> sut = UnitResult.Succeed<Error>().AsTask();
-            Func<Unit, Result<string, Error>> successFunction = null!;
+            Func<Unit, Result<string, Error>> bindFunction = null!;
 
             // Act + Assert
-            await sut.Invoking(res => res.Bind(successFunction))
+            await sut.Invoking(res => res.Bind(bindFunction))
                 .Should()
                 .ThrowAsync<ArgumentNullException>();
         }
 
         [Fact]
-        public async Task Rethrows_success_function_exception()
+        public async Task Rethrows_bind_function_exception()
         {
             Task<Result<Unit, Error>> sut = UnitResult.Succeed<Error>().AsTask();
             var exception = new InvalidOperationException();
-            Func<Unit, Result<string, Error>> successFunction = _ => throw exception;
+            Func<Unit, Result<string, Error>> bindFunction = _ => throw exception;
 
-            var result = await sut.Invoking(res => res.Bind(successFunction))
+            var result = await sut.Invoking(res => res.Bind(bindFunction))
                 .Should()
                 .ThrowAsync<InvalidOperationException>();
             result.Which.Should().Be(exception);
         }
 
         [Fact]
-        public async Task Returns_success_function_result()
+        public async Task Returns_bind_function_result()
         {
             Task<Result<Unit, Error>> sut = UnitResult.Succeed<Error>().AsTask();
-            Func<Unit, Result<string, Error>> successFunction = _ => Result.Succeed(Success);
+            Func<Unit, Result<string, Error>> bindFunction = _ => Result.Succeed(Success);
 
-            var result = await sut.Bind(successFunction);
+            var result = await sut.Bind(bindFunction);
 
             AssertSuccess(result, Success);
         }
 
         [Fact]
-        public async Task Returns_success_function_failure()
+        public async Task Returns_bind_function_failure()
         {
             Task<Result<Unit, Error>> sut = UnitResult.Succeed<Error>().AsTask();
-            Func<Unit, Result<string, Error>> successFunction = _ => Result.Fail(DefaultError);
+            Func<Unit, Result<string, Error>> bindFunction = _ => Result.Fail(DefaultError);
 
-            var result = await sut.Bind(successFunction);
+            var result = await sut.Bind(bindFunction);
 
             AssertFailure(result, DefaultError);
         }
@@ -197,9 +194,9 @@ namespace Rop.NET.Tests
         public async Task Returns_initial_failure()
         {
             Task<Result<Unit, Error>> sut = UnitResult.Fail(UnexpectedError).AsTask();
-            Func<Unit, Result<string, Error>> successFunction = _ => Result.Succeed(Success);
+            Func<Unit, Result<string, Error>> bindFunction = _ => Result.Succeed(Success);
 
-            var result = await sut.Bind(successFunction);
+            var result = await sut.Bind(bindFunction);
 
             AssertFailure(result, UnexpectedError);
         }
@@ -211,58 +208,56 @@ namespace Rop.NET.Tests
         public async Task Throws_exception_when_task_result_is_null()
         {
             Task<Result<Unit, Error>> sut = null!;
-            Func<Unit, Task<Result<string, Error>>> successFunctionAsync = null!;
+            Func<Unit, Task<Result<string, Error>>> bindFunctionAsync = null!;
 
-            var task = () => sut.BindAsync(successFunctionAsync);
+            var task = () => sut.BindAsync(bindFunctionAsync);
 
             await task.Should()
                 .ThrowAsync<ArgumentNullException>();
         }
 
         [Fact]
-        public async Task Throws_exception_when_success_function_is_null()
+        public async Task Throws_exception_when_bind_function_is_null()
         {
             Task<Result<Unit, Error>> sut = UnitResult.Succeed<Error>().AsTask();
-            Func<Unit, Task<Result<string, Error>>> successFunctionAsync = null!;
+            Func<Unit, Task<Result<string, Error>>> bindFunctionAsync = null!;
 
-            await sut.Invoking(res => res.BindAsync(successFunctionAsync))
+            await sut.Invoking(res => res.BindAsync(bindFunctionAsync))
                 .Should()
                 .ThrowAsync<ArgumentNullException>();
         }
 
         [Fact]
-        public async Task Rethrows_success_function_exception()
+        public async Task Rethrows_bind_function_exception()
         {
             Task<Result<Unit, Error>> sut = UnitResult.Succeed<Error>().AsTask();
             var exception = new InvalidOperationException();
-            Func<Unit, Task<Result<string, Error>>> successFunctionAsync = _ => throw exception;
+            Func<Unit, Task<Result<string, Error>>> bindFunctionAsync = _ => throw exception;
 
-            var result = await sut.Invoking(res => res.BindAsync(successFunctionAsync))
+            var result = await sut.Invoking(res => res.BindAsync(bindFunctionAsync))
                 .Should()
                 .ThrowAsync<InvalidOperationException>();
             result.Which.Should().Be(exception);
         }
 
         [Fact]
-        public async Task Returns_success_function_result()
+        public async Task Returns_bind_function_result()
         {
             Task<Result<Unit, Error>> sut = UnitResult.Succeed<Error>().AsTask();
-            Func<Unit, Task<Result<string, Error>>> successFunctionAsync =
-                _ => Result.Succeed<string, Error>(Success).AsTask();
+            Func<Unit, Task<Result<string, Error>>> bindFunctionAsync = _ => Result.Succeed<string, Error>(Success).AsTask();
 
-            var result = await sut.BindAsync(successFunctionAsync);
+            var result = await sut.BindAsync(bindFunctionAsync);
 
             AssertSuccess(result, Success);
         }
 
         [Fact]
-        public async Task Returns_success_function_failure()
+        public async Task Returns_bind_function_failure()
         {
             Task<Result<Unit, Error>> sut = UnitResult.Succeed<Error>().AsTask();
-            Func<Unit, Task<Result<string, Error>>> successFunctionAsync =
-                _ => Result.Fail<string, Error>(DefaultError).AsTask();
+            Func<Unit, Task<Result<string, Error>>> bindFunctionAsync = _ => Result.Fail<string, Error>(DefaultError).AsTask();
 
-            var result = await sut.BindAsync(successFunctionAsync);
+            var result = await sut.BindAsync(bindFunctionAsync);
 
             AssertFailure(result, DefaultError);
         }
@@ -271,10 +266,9 @@ namespace Rop.NET.Tests
         public async Task Returns_initial_failure()
         {
             Task<Result<Unit, Error>> sut = UnitResult.Fail(UnexpectedError).AsTask();
-            Func<Unit, Task<Result<string, Error>>> successFunctionAsync =
-                _ => Result.Succeed<string, Error>(Success).AsTask();
+            Func<Unit, Task<Result<string, Error>>> bindFunctionAsync = _ => Result.Succeed<string, Error>(Success).AsTask();
 
-            var result = await sut.BindAsync(successFunctionAsync);
+            var result = await sut.BindAsync(bindFunctionAsync);
 
             AssertFailure(result, UnexpectedError);
         }
